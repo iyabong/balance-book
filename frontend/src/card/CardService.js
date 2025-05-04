4abd23bd590f48afc5d88dcf48c97bf5542fe87d
iyabong <iyabong@gmail.com>
2025-05-04 17:30:36 +0900

    add console log

--- frontend/src/card/CardService.js
+++ frontend/src/card/CardService.js
@@ -3,6 +3,8 @@ const API_BASE = process.env.NODE_ENV === 'development'
                : process.env.REACT_APP_API_BASE_URL;
 
 export const getAllCards = async() => {
+    console.log('process.env.NODE_ENV:' + process.env.NODE_ENV);
+    console.log('API_BASE:' + API_BASE);
     const res = await fetch(`${API_BASE}/api/card`);
     if (!res.ok) {
         throw new Error('카드 목록 불러오기 실패');
