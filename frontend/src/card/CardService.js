7ba7e7009b42d9a351c945fb8ac50f566f26be18
iyabong <iyabong@gmail.com>
2025-05-04 17:09:24 +0900

    applying env variable

--- frontend/src/card/CardService.js
+++ frontend/src/card/CardService.js
@@ -1,5 +1,9 @@
+const API_BASE = process.env.NODE_ENV === 'development'
+               ? 'http://localhost:5000'
+               : process.env.REACT_APP_API_BASE_URL;
+
 export const getAllCards = async() => {
-    const res = await fetch('http://localhost:5000/api/card');
+    const res = await fetch(`${API_BASE}/api/card`);
     if (!res.ok) {
         throw new Error('카드 목록 불러오기 실패');
     }
