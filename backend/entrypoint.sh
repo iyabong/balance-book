7b2b0a8a809aec8811327b56ad02571c6000e13f
iyabong <68013311+iyabong@users.noreply.github.com>
2025-06-24 23:22:09 +0900

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,9 @@ echo "📦 OCI Wallet 복원 중..."
 echo "$WALLET_B64" | base64 -d > /app/wallet.zip
 unzip -o /app/wallet.zip -d /app/wallet
 
+# 👉 현재 컨테이너 외부 IP 출력
+echo "🌍 현재 외부 IP:"
+curl ifconfig.me || wget -qO- ifconfig.me
+
 echo "🚀 .NET 앱 실행"
 dotnet BalanceBook.dll
