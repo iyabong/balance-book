1a634e1b4cbb3988897d1d8eae19a93b57463431
iyabong <68013311+iyabong@users.noreply.github.com>
2025-06-25 23:12:14 +0900

    Update entrypoint_render.sh

--- backend/entrypoint_render.sh
+++ backend/entrypoint_render.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#ENTRYPOINT for Render
+set -x
 
-echo "📦 [Render] OCI Wallet 복원 중..."
+echo "📦 [Render] OCI Wallet 복원 중..." >&2
 unzip /etc/secrets/Wallet_A.zip -d /app/wallet
 
-# 👉 현재 컨테이너 외부 IP 출력
-echo "🌍 현재 외부 IP:"
+echo "📁 wallet 폴더 목록:" >&2
+ls -l /app/wallet
+
+echo "🌍 현재 외부 IP:" >&2
 curl ifconfig.me || wget -qO- ifconfig.me
 
-echo "🚀 .NET 앱 실행"
+echo "🚀 .NET 앱 실행" >&2
 dotnet BalanceBook.dll
